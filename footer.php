39de5f178896b47692d8879757c82ebe205f4a2a
sugeesh <lpdneo@gmail.com>
2016-03-03 19:44:11 +0530

    2K16 corrected

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="back-to-top">
             <img alt="" src="images/back-to-top.png">
 
-            <p>&copy; Copyright IDEA Challenge 2016 | All Rights Reserved</p>
+            <p>&copy; Copyright IDEA Challenge 2K16 | All Rights Reserved</p>
         </div>
     </div>
 </footer>
